bf2ff3d11f2621f4d6d8f8dc7002042ae4281738
Dharmesh Singh <dharmeshsinghpaliwal.7@gmail.com>
2021-02-14 19:34:09 +0530

    manage reconfigured

--- projectCRIO/manage.py
+++ projectCRIO/manage.py
@@ -4,6 +4,7 @@ import os
 import sys
 from django.core.management.commands.runserver import Command as runserver
 def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectCRIO.settings')
     runserver.default_port="8081"
     try:
         from django.core.management import execute_from_command_line
