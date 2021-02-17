4b8782d4f71df7c359c9cadbed8bdca37f4dc914
Dharmesh Singh <dharmeshsinghpaliwal.7@gmail.com>
2021-02-17 08:24:01 +0000

    Update urls.py

--- projectCRIO/appCRIO/urls.py
+++ projectCRIO/appCRIO/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings # new
 app_name='appCRIO'
 urlpatterns=[
     path('',views.memeListAPI,name='addDisplayMemes'),
-    path('/',views.memeListAPI,name='addDisplayMemes'),
     path('<int:pk>/',views.memeDetailAPI,name='editShowMeme'),
     path('memeDelete/<int:pk>/', views.memeDelete,name='deleteMeme'),
 ]
