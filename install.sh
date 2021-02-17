43c8de8cc8d4f9ef899d5f9e7b5d74eddb27fd7b
Dharmesh Singh <dharmeshsinghpaliwal.7@gmail.com>
2021-02-17 06:58:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo apt install -y python3-pip
-sudo pip3 install virtualenvwrapper
+pip3 install virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS=' -p /usr/bin/python3 '
 source /usr/local/bin/virtualenvwrapper.sh
 source ~/.bashrc
-sudo pip3 install virtualenv
-sudo mkvirtualenv --python=python3.6.9  ~/venv_django
-sudo apt install -y python3-pip
-sudo pip3 install -r requirements.txt
+pip3 install virtualenv
+mkvirtualenv --python=python3.6.9  ~/venv_django
+pip3 install -r requirements.txt
