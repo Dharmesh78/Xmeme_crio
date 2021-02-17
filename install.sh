fcaf70f5a4ab4ad0443580ad289d01cbb3ecd666
Dharmesh Singh <dharmeshsinghpaliwal.7@gmail.com>
2021-02-17 13:39:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt install -y python3-pip
 sudo ln /usr/bin/python3 /usr/bin/python
 #to handle the meme endpoint issue in django
-sudo echo "APPEND_SLASH=False" >> ~/dharmeshsinghpaliwal-7-me_buildout_xmeme/projectCRIO/projectCRIO/settings.py
+#sudo echo "APPEND_SLASH=False" >> ~/dharmeshsinghpaliwal-7-me_buildout_xmeme/projectCRIO/projectCRIO/settings.py
 
 pip3 install -r requirements.txt
