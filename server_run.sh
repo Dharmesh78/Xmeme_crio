307b135c3b3537046003fe9af8b8c082f96fcfca
Dharmesh Singh <dharmeshsinghpaliwal.7@gmail.com>
2021-02-17 07:08:27 +0000

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #source ~/venv_django/bin/activate
 pip3 install -r requirements.txt
-cd projectCRIO/projectCRIO
+cd dharmeshsinghpaliwal-7-me_buildout_xmeme/projectCRIO/projectCRIO
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
