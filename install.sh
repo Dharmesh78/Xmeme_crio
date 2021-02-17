39f099a0248c9b874af93555637f113a831745e5
Dharmesh Singh <dharmeshsinghpaliwal.7@gmail.com>
2021-02-17 14:20:38 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 sudo apt install -y python3-pip
 sudo ln /usr/bin/python3 /usr/bin/python
 #to handle the meme endpoint issue in django
-sudo echo "urlpatterns.append(path('memes/',include('appCRIO.urls')))" >> ~/dharmeshsinghpaliwal-7-me_buildout_xmeme/projectCRIO/projectCRIO/urls.py
+sudo echo "urlpatterns.append(path('memes',include('appCRIO.urls')))" >> ~/dharmeshsinghpaliwal-7-me_buildout_xmeme/projectCRIO/projectCRIO/urls.py
 pip3 install -r requirements.txt
